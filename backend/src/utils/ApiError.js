4c87f07bcdc08e37fea26621c2c99b660f4b85bc
AtifAhmedShaikh <atifahmad2219@gmail.com>
2024-01-22 06:33:15 -0800

    custom classes: for ApiError and ApiResponse

---
+++ backend/src/utils/ApiError.js
@@ -0,0 +1,23 @@
+/**
+ * Represents an API error.
+ *
+ * @class ApiError
+ * @extends {Error}
+ */
+class ApiError extends Error {
+  /**
+   * Creates an instance of ApiError.
+   *
+   * @param {number} statusCode - The HTTP status code.
+   * @param {string} message - The error message.
+   */
+  constructor(statusCode, message) {
+    super();
+    this.statusCode = statusCode;
+    this.message = message;
+    this.data = null;
+    this.success = false;
+  }
+}
+
+export default ApiError;
---
+++ backend/src/utils/ApiResponse.js
@@ -0,0 +1,19 @@
+/**
+ * A custom class to send consist response in every HTTP request
+ * @class ApiResponse
+ */
+class ApiResponse {
+  /**
+   * @param {number} statusCode - HTTP status codes send in response
+   * @param {object} data - the data to send in response
+   * @param {string} message - the message to associated with the response.
+   */
+  constructor (statusCode, data, message) {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = statusCode < 400;
+  }
+}
+
+export default ApiResponse;
