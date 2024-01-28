6645d7769be70599393578e6f1f0ca6081cf1b6c
AtifAhmedShaikh <atifahmad2219@gmail.com>
2024-01-28 10:22:37 -0800

    fix: fix verify token method and update isAuthorizedToDelete middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,5 +1,6 @@
 import { ACCESS_TOKEN_SECRET } from "../config/envConfig.js";
 import { findPostById } from "../services/post.service.js";
+import { findTweetById } from "../services/tweet.service.js";
 import { findUserById } from "../services/user.service.js";
 import ApiError from "../utils/ApiError.js";
 import asyncHandler from "../utils/asyncHandler.js";
@@ -7,35 +8,58 @@ import Jwt from "jsonwebtoken";
 
 // middleware to authenticate user based on provided access token using Jwt
 export const authenticateUser = asyncHandler(async (req, res, next) => {
-  // extract token from cookies or request header
-  const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
-  if (!token) {
-    throw new ApiError(401, "unauthorized request, token not found !");
+  try {
+    // extract token from cookies or request header
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      throw new ApiError(401, "unauthorized request, token not found !");
+    }
+    const decoded =await Jwt.verify(token, ACCESS_TOKEN_SECRET);
+    console.log(decoded);
+    // fetch the user from the database based on the decoded user ID
+    const user = await findUserById(decoded?._id);
+    if (!user) {
+      throw new ApiError(401, "unauthorized request, Invalid access token !");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, "unauthorized request, access token has expired !", error);
   }
-  const decoded = Jwt.verify(token, ACCESS_TOKEN_SECRET);
-  // fetch the user from the database based on the decoded user ID
-  const user = await findUserById(decoded?._id);
-  if (!user) {
-    throw new ApiError(401, "unauthorized request, Invalid access token !");
-  }
-  req.user = user;
-  next();
 });
 
 // middleware to ensure user has authorized to delete post,reel,tweet,comment
 export const isAuthorizedToDelete = deletingDocumentType => {
   return asyncHandler(async (req, res, next) => {
-    const documentIDToBeDeleted = req.params?.id; // extract id of one of them post,reel,tweet,comments
-    const userId = req.user?._id;
+    const documentId = req.params.id; // extract document Id of  post,reel,tweet,comments
+    const userId = req.user?._id; // extract loggedIn user Id
+    // If user has delete his post
     if (deletingDocumentType === "POST") {
-      const post = await findPostById(documentIDToBeDeleted);
-      // check the creator is current user or not, convert mongoose ObjectId into string
-      if (String(post?.creator) === String(userId)) return next(); // user is deleting own document
+      const postToBeDeleted = await findPostById(documentId);
+      if (!postToBeDeleted) {
+        throw new ApiError(404, "post not found, Invalid post Id");
+      }
+      // check the post creator is current user or not, convert mongoose ObjectId into string
+      if (String(postToBeDeleted?.creator?._id) === String(userId)) {
+        return next(); // pass control into next handler
+      }
       throw new ApiError(
         401,
         `you unauthorized to delete this ${deletingDocumentType?.toLowerCase()}`,
       );
-    } else if (deletingDocumentType === "REEL") {
+    } else if (deletingDocumentType === "TWEET") {
+      const tweetToBeDeleted = await findTweetById(documentId);
+      if (!tweetToBeDeleted) {
+        throw new ApiError(404, "tweet not found, Invalid post Id");
+      }
+      // check the tweet creator is current user or not, convert mongoose ObjectId into string
+      if (String(tweetToBeDeleted?.author?._id) === String(userId)) {
+        return next(); // pass control into next handler
+      }
+      throw new ApiError(
+        401,
+        `you unauthorized to delete this ${deletingDocumentType?.toLowerCase()}`,
+      );
     }
   });
 };
