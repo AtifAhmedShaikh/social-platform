a8ab37fa2b3e5d64998895bc91af8eda75f18afd
AtifAhmedShaikh <atifahmad2219@gmail.com>
2024-01-31 10:33:40 -0800

    add reels router in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,7 @@ import postRouter from "./routes/post.routes.js";
 import likeRouter from "./routes/like.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
+import reelRouter from "./routes/reel.routes.js";
 
 const app = express();
 
@@ -31,6 +32,7 @@ app.use("/api/v1/posts", postRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/reels", reelRouter);
 
 // Handle not found routes
 app.use((req, res) => {
