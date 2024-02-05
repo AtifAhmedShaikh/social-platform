d67e0cd05a9589115fb4eda23a3e2841bc9c2789
AtifAhmedShaikh <atifahmad2219@gmail.com>
2024-02-05 12:51:35 -0800

    feat: Implement comments functionality on  posts reels and tweets

--- backend/src/controllers/comment.controller.js
+++ backend/src/controllers/comment.controller.js
@@ -1,16 +1,117 @@
 import asyncHandler from "../utils/asyncHandler.js";
+import CommentModel from "../models/Comment.model.js";
+import ApiResponse from "../utils/ApiResponse.js";
+import { findPostComments } from "../services/post.service.js";
+import { findReelComments } from "../services/reel.service.js";
+import { findTweetComments } from "../services/tweet.service.js";
 
-const addCommentOnPost = asyncHandler(() => {});
-const addCommentOnReel = asyncHandler(() => {});
-const addCommentOnTweet = asyncHandler(() => {});
+const addCommentOnPost = asyncHandler(async (req, res) => {
+  const postId = req.params?.id;
+  const userId = req.user?._id;
+  const { content } = req.body; // content or message of comment
+  const createdComment = await CommentModel.create({
+    author: userId,
+    post: postId,
+    content,
+  });
+  const responseInstance = new ApiResponse(
+    201,
+    { comment: createdComment },
+    "comment created successfully on post !",
+  );
+  res.status(200).json(responseInstance);
+});
+const addCommentOnReel = asyncHandler(async (req, res) => {
+  const reelId = req.params?.id;
+  const userId = req.user?._id;
+  const { content } = req.body;
+  const createdComment = await CommentModel.create({
+    author: userId,
+    reel: reelId,
+    content,
+  });
+  const responseInstance = new ApiResponse(
+    201,
+    { comment: createdComment },
+    "comment created successfully on reel !",
+  );
+  res.status(200).json(responseInstance);
+});
+const addCommentOnTweet = asyncHandler(async (req, res) => {
+  const tweetId = req.params?.id;
+  const userId = req.user?._id;
+  const { content } = req.body;
+  const createdComment = await CommentModel.create({
+    author: userId,
+    tweet: tweetId,
+    content,
+  });
+  const responseInstance = new ApiResponse(
+    201,
+    { comment: createdComment },
+    "comment created successfully on tweet !",
+  );
+  res.status(200).json(responseInstance);
+});
 
-const updatePostComment = asyncHandler(() => {});
-const updateReelComment = asyncHandler(() => {});
-const updateTweetComment = asyncHandler(() => {});
+// get comment Id from params and update its content by provided content
+const updateComment = asyncHandler(async (req, res) => {
+  const commentId = req.params?.id;
+  const { content } = req.body;
+  const updatedComment = await CommentModel.findByIdAndUpdate(
+    commentId,
+    { content },
+    { new: true },
+  );
+  const responseInstance = new ApiResponse(
+    200,
+    { comment: updatedComment },
+    "comment updated successfully on post!",
+  );
+  res.status(200).json(responseInstance);
+});
 
-const getPostComments = asyncHandler(() => {});
-const getReelComments = asyncHandler(() => {});
-const getTweetComments = asyncHandler(() => {});
+const deleteComment = asyncHandler(async (req, res) => {
+  const commentId = req.params.id;
+  await CommentModel.findByIdAndDelete(commentId);
+  const responseInstance = new ApiResponse(200, {}, "comment has deleted successfully !");
+  res.status(200).json(responseInstance);
+});
+
+const getPostComments = asyncHandler(async (req, res) => {
+  const postId = req.params.id;
+  const userId = req.user?._id;
+  const postComments = await findPostComments(postId, userId);
+  const responseInstance = new ApiResponse(
+    200,
+    { comments: postComments },
+    "comments fetched successfully",
+  );
+  res.status(200).json(responseInstance);
+});
+
+const getReelComments = asyncHandler(async (req, res) => {
+  const reelId = req.params.id;
+  const userId = req.user?._id;
+  const reelComments = await findReelComments(reelId, userId);
+  const responseInstance = new ApiResponse(
+    200,
+    { comments: reelComments },
+    "comments fetched successfully",
+  );
+  res.status(200).json(responseInstance);
+});
+const getTweetComments = asyncHandler(async (req, res) => {
+  const tweetId = req.params.id;
+  const userId = req.user?._id;
+  const tweetComments = await findTweetComments(tweetId, userId);
+  const responseInstance = new ApiResponse(
+    200,
+    { comments: tweetComments },
+    "comments fetched successfully",
+  );
+  res.status(200).json(responseInstance);
+});
 
 export {
   // Add Comment
@@ -21,8 +122,7 @@ export {
   getPostComments,
   getReelComments,
   getTweetComments,
-  // Update comments
-  updatePostComment,
-  updateReelComment,
-  updateTweetComment,
+  // Update and delete comments
+  updateComment,
+  deleteComment,
 };
--- backend/src/routes/comment.routes.js
+++ backend/src/routes/comment.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { authenticateUser } from "../middleware/auth.middleware.js";
+import { authenticateUser, isAuthorizedToDelete } from "../middleware/auth.middleware.js";
 import {
   getReelComments,
   getPostComments,
@@ -7,21 +7,17 @@ import {
   addCommentOnPost,
   addCommentOnReel,
   addCommentOnTweet,
-  updatePostComment,
-  updateReelComment,
-  updateTweetComment,
+  updateComment,
+  deleteComment,
 } from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
 router.use(authenticateUser);
 
-router.route("/post/:id").get(getPostComments).post(addCommentOnPost).patch(updatePostComment);
-router.route("/reel/:id").get(getReelComments).post(addCommentOnReel).patch(updateReelComment);
-router
-  .route("/tweet/:id")
-  .get(getTweetComments)
-  .post(addCommentOnTweet)
-  .patch(updateTweetComment);
+router.route("/post/:id").get(getPostComments).post(addCommentOnPost);
+router.route("/reel/:id").get(getReelComments).post(addCommentOnReel);
+router.route("/tweet/:id").get(getTweetComments).post(addCommentOnTweet);
+router.route("/:id").put(updateComment).delete(isAuthorizedToDelete("COMMENT"), deleteComment);
 
 export default router;
