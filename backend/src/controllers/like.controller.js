c20883d5fb9922dba71649f559a550d3c23fc2f0
AtifAhmedShaikh <atifahmad2219@gmail.com>
2024-02-07 12:28:27 -0800

    validate Object Id and Ensure this exists in database before like

--- backend/src/controllers/like.controller.js
+++ backend/src/controllers/like.controller.js
@@ -1,15 +1,23 @@
 import LikeModel from "../models/Like.model.js";
+import CommentModel from "../models/Comment.model.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiResponse from "../utils/ApiResponse.js";
-
+import ApiError from "../utils/ApiError.js";
+import { findPostById } from "../services/post.service.js";
+import { findReelById } from "../services/reel.service.js";
+import { findTweetById } from "../services/tweet.service.js";
 // Toggle controllers for like, if already liked by current user so unlike it otherwise like
 
 const togglePostLike = asyncHandler(async (req, res) => {
   const postId = req.params.id;
   const userId = req.user?._id;
+  const targetedPostToLike = await findPostById(postId, userId);
+  if (!targetedPostToLike) {
+    throw new ApiError(404, "post not found!, Invalid post Id ");
+  }
   const hasLiked = await LikeModel.findOne({ post: postId, likedBy: userId });
   if (hasLiked) {
-    await LikeModel.deleteOne({ post: postId, likedBy: userId });
+    await LikeModel.findOneAndDelete({ post: postId, likedBy: userId });
   } else {
     await LikeModel.create({ post: postId, likedBy: userId });
   }
@@ -24,9 +32,13 @@ const togglePostLike = asyncHandler(async (req, res) => {
 const toggleReelLike = asyncHandler(async (req, res) => {
   const reelId = req.params.id;
   const userId = req.user?._id;
+  const targetedReelToLike = await findReelById(reelId, userId);
+  if (!targetedReelToLike) {
+    throw new ApiError(404, "reel not found!, Invalid reel Id ");
+  }
   const hasLiked = await LikeModel.findOne({ reel: reelId, likedBy: userId });
   if (hasLiked) {
-    await LikeModel.deleteOne({ reel: reelId, likedBy: userId });
+    await LikeModel.findOneAndDelete({ reel: reelId, likedBy: userId });
   } else {
     await LikeModel.create({ reel: reelId, likedBy: userId });
   }
@@ -41,9 +53,14 @@ const toggleReelLike = asyncHandler(async (req, res) => {
 const toggleTweetLike = asyncHandler(async (req, res) => {
   const tweetId = req.params.id;
   const userId = req.user?._id;
+  const targetedTweetToLike = await findTweetById(tweetId, userId);
+  if (!targetedTweetToLike) {
+    throw new ApiError(404, "tweet not found!, Invalid tweet Id ");
+  }
+
   const hasLiked = await LikeModel.findOne({ tweet: tweetId, likedBy: userId });
   if (hasLiked) {
-    await LikeModel.deleteOne({ tweet: tweetId, likedBy: userId });
+    await LikeModel.findOneAndDelete({ tweet: tweetId, likedBy: userId });
   } else {
     await LikeModel.create({ tweet: tweetId, likedBy: userId });
   }
@@ -58,9 +75,13 @@ const toggleTweetLike = asyncHandler(async (req, res) => {
 const toggleCommentLike = asyncHandler(async (req, res) => {
   const commentId = req.params.id;
   const userId = req.user?._id;
+  const targetedCommentToLike = await CommentModel.findById(commentId);
+  if (!targetedCommentToLike) {
+    throw new ApiError(404, "comment not found!, Invalid comment Id ");
+  }
   const hasLiked = await LikeModel.findOne({ comment: commentId, likedBy: userId });
   if (hasLiked) {
-    await LikeModel.deleteOne({ comment: commentId, likedBy: userId });
+    await LikeModel.findOneAndDelete({ comment: commentId, likedBy: userId });
   } else {
     await LikeModel.create({ comment: commentId, likedBy: userId });
   }
@@ -71,6 +92,5 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
   );
   res.status(200).json(responseInstance);
 });
-
 
 export { togglePostLike, toggleCommentLike, toggleTweetLike, toggleReelLike };
--- backend/src/models/Like.model.js
+++ backend/src/models/Like.model.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
-// model to user history of tracking to save any post,tweet or reels
+import UserHistory from "./UserHistory.model.js";
+
 const likeSchema = new mongoose.Schema(
   {
-    // user who is like the post or reel..
     likedBy: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "users",
@@ -29,6 +29,55 @@ const likeSchema = new mongoose.Schema(
     timestamps: true,
   },
 );
+
+// Middleware to add or remove liked posts, reels, tweets from user liked History
+likeSchema.post("save", async function (doc, next) {
+  try {
+    // If user likes the post, add this post to the user's liked history
+    if (this.post) {
+      await UserHistory.findOneAndUpdate({ owner: this.likedBy }, { $push: { posts: this.post } });
+    } else if (this.reel) {
+      await UserHistory.findOneAndUpdate({ owner: this.likedBy }, { $push: { reels: this.reel } });
+    } else if (this.tweet) {
+      await UserHistory.findOneAndUpdate(
+        { owner: this.likedBy },
+        { $push: { tweets: this.tweet } },
+      );
+    }
+
+    next();
+  } catch (error) {
+    console.error("Error in post save middleware:");
+    next(error);
+  }
+});
+
+likeSchema.pre("findOneAndDelete", async function (next) {
+  try {
+    const docToUpdate = await this.model.findOne(this.getQuery()); // access of currently like document
+    // If user unlike the post, remove this post from the user's liked history
+    if (docToUpdate.post) {
+      await UserHistory.findOneAndUpdate(
+        { owner: docToUpdate.likedBy },
+        { $pull: { posts: docToUpdate.post } },
+      );
+    } else if (docToUpdate.reel) {
+      await UserHistory.findOneAndUpdate(
+        { owner: docToUpdate.likedBy },
+        { $pull: { reels: docToUpdate.reel } },
+      );
+    } else if (docToUpdate.tweet) {
+      await UserHistory.findOneAndUpdate(
+        { owner: docToUpdate.likedBy },
+        { $pull: { tweets: docToUpdate.tweet } },
+      );
+    }
+    next();
+  } catch (error) {
+    console.error("Error in pre findOneAndDelete middleware:");
+    next(error);
+  }
+});
 
 const LikeModel = mongoose.model("like", likeSchema);
 export default LikeModel;
