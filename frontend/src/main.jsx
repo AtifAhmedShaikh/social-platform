770d2502b7b3f908a988b2494f3cef0fcd8d86b7
AtifAhmedShaikh <atifahmad2219@gmail.com>
2024-02-12 11:24:17 -0800

    Add Error boundary in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import { ErrorBoundary } from "react-error-boundary";
 import Router from "./router/Router.jsx";
+import FallbackComponent from "./pages/Error/ErrorPage.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Router/>
-    </BrowserRouter>
+    <ErrorBoundary FallbackComponent={FallbackComponent}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>
 );
