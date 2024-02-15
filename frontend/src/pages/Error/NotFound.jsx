e829f62e9c743e154d75101478003d9e884857ba
AtifAhmedShaikh <atifahmad2219@gmail.com>
2024-02-15 10:58:02 -0800

    delete unnecessary  files and update NotFound page

--- frontend/src/pages/Error/NotFound.jsx
+++ frontend/src/pages/Error/NotFound.jsx
@@ -1,17 +1,16 @@
-import {Button} from "@/components/ui/button"
-import { useNavigate } from "react-router-dom"
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
-  const navigate=useNavigate()
+  const navigate = useNavigate();
   return (
     <div className="h-screen w-100 flex justify-center items-center flex-col">
       <div className="px-10 py-5 text-black">
         <h2 className="text-2xl">404 Not Found !</h2>
       </div>
-      <Button onclick={() => navigate("/")}>Go back to  Home page </Button>
+      <Button onClick={() => navigate("/")}>Go back to Home page </Button>
     </div>
+  );
+};
 
-  )
-}
-
-export default NotFound
+export default NotFound;
