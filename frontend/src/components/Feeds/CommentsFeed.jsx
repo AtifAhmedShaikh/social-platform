14f7a43b555b034703a3f6571ffc79b6afb417ad
AtifAhmedShaikh <atifahmad2219@gmail.com>
2024-02-15 11:05:56 -0800

    Add CommentsFeedPage

---
+++ frontend/src/components/Feeds/CommentsFeed.jsx
@@ -0,0 +1,18 @@
+import CommentCard from "@/components/Cards/CommentCard";
+
+const CommentsFeed = () => {
+  return (
+    <div className="flex flex-col mt-7 px-2">
+      <CommentCard />
+      <CommentCard />
+      <CommentCard />
+      <CommentCard />
+      <CommentCard />
+      <CommentCard />
+      <CommentCard />
+      <CommentCard />
+    </div>
+  );
+};
+
+export default CommentsFeed;
